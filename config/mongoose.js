5c3b1587778d4fe9cf73aba883978c9a2715b5f2
Avineet Bhat <surajbhat316@gmail.com>
2023-07-23 13:03:48 +0530

    Created dotenv file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/bug_tracker_app');
-
+// mongoose.connect('mongodb://127.0.0.1:27017/bug_tracker_app');
+mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
 db.on('error',console.error.bind(console, "Error Connecting To Db"));
 
